50910ab4356ab4c558f98009e8a16ac7185cac00
Haider Ali Shah <headeralishah@gmail.com>
2016-09-28 21:36:20 +0500

    CSV TO JSON from Web improved.

--- CSVLoading.js
+++ CSVLoading.js
@@ -1,42 +1,46 @@
-var csv = require('csv-to-json');
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({ constructResult: false }); //for big csv data
 var mongoose = require('mongoose');
-
 //you need to change the database path 
-var connection = mongoose.connect("mongodb://localhost/test");
-
+var connection = mongoose.connect("mongodb://localhost/testCSV_API");
 //you may alter the data schema
 var CSVdataSchema = new mongoose.Schema({
-    data: { type: JSON, required: true }
+    x: { type: Number, required: true },
+    y: { type: Number, required: true },
+    country: { type: String, required: true },
+    population: { type: Number, required: true },
+    label: { type: String, required: true }
 });
-
 var CSVToJSONDataModel = mongoose.model("CSVToJSONDataModel", CSVdataSchema);
 function loading(app) {
     app.post("/saveCSVInJson", saveCSVInJson);
     function saveCSVInJson(req, res) {
-        //console.log(req.body);
-        saveDataToMongoDB("./cities.csv", function (json) {
+        var fPath = req.body.data.url;
+        saveDataToMongoDB(fPath, function (json) {
             res.end();
         });
     }
     function saveDataToMongoDB(filePath, cb) {
-        var obj = {
-            filename: filePath
-        };
-        var callback = function (err, json) {
+        require("request").get(filePath).pipe(converter);
+        converter.on("record_parsed", function (jsonObj) {
+            console.log(jsonObj); //here is your result json object
             var JSONedData = new CSVToJSONDataModel({
-                data: json
+                y: jsonObj.y,
+                x: jsonObj.x, 
+                country: jsonObj.country,
+                population: jsonObj.population,
+                label: jsonObj.label
             });
             JSONedData.save(function (err, success) {
                 if (err) {
-                    console.log(err);
+                    console.log(err + "Error! Something went wrong.");
                 }
                 else {
                     console.log(success + "JSON converted data saved");
                 }
             });
-            cb(json);
-        };
-        csv.parse(obj, callback);
-    }
+
+            });
+        }
 }
-exports.initializeModels = loading;
+    exports.initializeModels = loading;
--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var csv = require('csv-to-json');
 var loading = require("./CSVLoading");
 var cors = require('cors');
 var app = express();
+var publicPath = path.resolve(__dirname + "/Web");
+
+app.use("/", express.static(publicPath));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.set("port", process.env.PORT || 3001);
+app.set("port", process.env.PORT || 3002);
 
 loading.initializeModels(app);
 
-app.listen(3000, function () {
-  console.log("App started on port 3000");
+app.listen(3002, function () {
+  console.log("App started on port 3002");
 });
